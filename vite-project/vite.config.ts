871d639960a4a0843b760bb913a512c132e401b9
AROMAL VG <aromalvijayan448@gmail.com>
2025-02-12 12:46:25 +0530

    Deploying to GitHub Pages

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,10 +1,21 @@
 
 import path from "path";
+import { resolve } from "path";
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from "vite";
  
 export default defineConfig({
+  base: "/portfolio/",
+
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "src/404.html"), // Ensure 404.html is included in the build
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
